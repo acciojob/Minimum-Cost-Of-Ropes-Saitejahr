8e0468ca537c4fdc59cd564fade77c4a26cfe613
SAI TEJA H R <98202897+Saitejahr@users.noreply.github.com>
2023-02-22 18:50:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,28 +1,16 @@
-function minCostToFormRope(a) {
-  //your code here
-	a.preventDefault();
-	var inputElement = document.querySelector("input").value;
-	var arr = inputElement.split(",");
-	arr.sort(function (a,b) {
-		return a-b;
-	});
-	var cost =0;
-	while (arr.length>1) {
-		var res = Number(arr[0]) + Number(arr[1]);
+function minCost(event) {
+	event.preventDefault();
+	var input = document.querySelector("input").value;
+	var arr = input.split(",");
+	var cost=0;
+	while(arr.length > 1){
+		arr.sort(function (a,b) {return a-b});
+		var temp=Number(arr[0]) + Number(arr[1]);
+		cost+=temp;
+		arr.push(temp);
 		arr.splice(0,2);
-		arr.push(res);
-		cost+=res;
-		arr.sort(function (a,b) {
-			return a-b;
-		});
-		
-		
-		
-		
+		arr.sort(function (a,b) {return a-b});
 		
 	}
-
-	document.getElementById("result").textContent = cost;
-  
-  
-} 
+	document.querySelector("div").textContent=cost;
+}
